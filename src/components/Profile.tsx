98b27312fe840aa47067ed416b533f44f087f4b2
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-02-23 11:39:13 -0300

    :sparkles: feat: profile

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import styles from '../styles/components/Profile.module.css';
+
+
+
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://avatars.githubusercontent.com/u/45343619?s=460&u=1929279b8eb8dbe2434c20e7ad7e239674b4cc17&v=4" alt="Héliton Oliveira" />
+
+      <div>
+        <strong>Héliton Oliveira</strong>
+        <p>
+          <img src="icons/level.svg" alt="level icon"/>
+          Level 1
+
+        </p>
+      </div>
+    </div>
+  );
+}
