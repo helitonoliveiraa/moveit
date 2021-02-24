d0a45c8683f1937ed9db5d663b95604487a73fca
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-02-24 10:02:17 -0300

    :recycle: refactory: countdow button

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/components/Countdown.module.css';
 
 export function Countdow() {
   const [time, setTime] = useState(25 * 60);
-  const [active, setActive] = useState(false);
+  const [isActive, setIsActive] = useState(false);
 
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
@@ -12,16 +12,16 @@ export function Countdow() {
   const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
 
   function toggleCoutdown() {
-    setActive(!active);
+    setIsActive(!isActive);
   }
 
   useEffect(() => {
-    if (active && time > 0) {
+    if (isActive && time > 0) {
       setTimeout(() => {
         setTime(time - 1);
       }, 1000);
     }
-  }, [active, time]);
+  }, [isActive, time]);
 
   return (
     <div>
@@ -39,24 +39,24 @@ export function Countdow() {
         </div>
       </div>
 
-      {!active ? (
+      {isActive ? (
         <button 
-          type="button" 
-          className={styles.countdownButton} 
-          onClick={toggleCoutdown}
-        >
-          Iniciar um ciclo
-          <img src="icons/play-arrow.svg" alt="Arrow play"/>
-        </button>
+        type="button" 
+        className={`${styles.countdownButton} ${styles.countdownButtonActive}`} 
+        onClick={toggleCoutdown}
+      >
+        Abandonar ciclo
+        <img src="icons/close.svg" alt="Close"/>
+      </button>
       ) : (
-        <button 
-          type="button" 
-          className={styles.countdownStopButton} 
-          onClick={toggleCoutdown}
-        >
-          Abandonar ciclo
-          <img src="icons/close.svg" alt="Close countdow"/>
-        </button>
+      <button 
+      type="button" 
+      className={styles.countdownButton} 
+      onClick={toggleCoutdown}
+      >
+      Iniciar um ciclo
+      <img src="icons/play-arrow.svg" alt="Play"/>
+      </button>
       )}
     </div>
   );
