4ba89c4ca21e9356eceae0a84d81db3688ce8752
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-02-24 09:35:07 -0300

    :sparkles: feat: challenge and countdow components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,31 @@
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdow } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+
+import Head from 'next/head';
+
+import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
+      <Head>
+        <title>Inicio | move.it</title>
+      </Head>
+
       <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+          <Countdow />
+        </div>
+        <div>
+          
+        </div>
+      </section>
     </div>
   )
 }
